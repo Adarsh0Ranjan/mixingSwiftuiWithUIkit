c85ca7a3d820a64734837f1088233924144230a0
adarsh <adarsh0ranjan@gmail.com>
2023-06-28 10:52:20 +0530

    navigation added with swift

--- StoryboardAndSwiftuiMixing/SwiftUIView2.swift
+++ StoryboardAndSwiftuiMixing/SwiftUIView2.swift
@@ -8,16 +8,66 @@
 import SwiftUI
 
 struct SwiftUIView2: View {
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
         NavigationView {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("Hello, World!")
+                Button("Dismiss") {
+                    presentationMode.wrappedValue.dismiss()
+                }
+                
+                NavigationLink(destination: NewView()) {
+                    Text("Go to New View")
+                }
+            }
         }
         .navigationBarBackButtonHidden(false)
     }
 }
+
 
 struct SwiftUIView2_Previews: PreviewProvider {
     static var previews: some View {
         SwiftUIView2()
     }
 }
+
+
+
+struct NewView: View {
+    @State private var isPresentingNewView = false
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack {
+            Text("This is the new view")
+            Button("Present New View") {
+                isPresentingNewView = true
+            }
+            Button("Dismiss") {
+                presentationMode.wrappedValue.dismiss()
+            }
+        }
+        .sheet(isPresented: $isPresentingNewView, onDismiss: {
+            // Optional: Perform actions when the presented view is dismissed
+        }) {
+            PresentedView()
+        }
+    }
+}
+
+struct PresentedView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack {
+            Text("Presented View")
+            Button("Dismiss") {
+                presentationMode.wrappedValue.dismiss()
+            }
+        }
+    }
+}
+
+
